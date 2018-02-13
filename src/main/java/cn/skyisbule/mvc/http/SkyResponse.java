4ab96867433c7422e744f39ef301202ac4a9dbf0
skyisbule <heqiut@qq.com>
2018-02-13 19:19:26 +0800

    基本为完成请求和返回处理，开始编写ioc模块。

--- src/main/java/cn/skyisbule/mvc/http/SkyResponse.java
+++ src/main/java/cn/skyisbule/mvc/http/SkyResponse.java
@@ -11,7 +11,6 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.HashSet;
 import java.util.Set;
 
-import static io.netty.handler.codec.http.HttpResponseStatus.OK;
 
 /**
  * Created by skyisbule on 2018/2/12.
@@ -27,30 +26,36 @@ public class SkyResponse {
     private ChannelHandlerContext ctx         = null;
     private CharSequence          contentType = null;
     private CharSequence          dateString  = null;
-    private String                content     = null;
+    private String                content     = " ";
 
     public void addHeader(String s,Object o){
         headers.add(s,o);
     }
 
-    public void addCookie(){
-
+    public void addCookie(Cookie cookie){
+        addHeader("Set-Cookie",cookie.toString());
     }
 
     public void write(){
-        content = "<html><body><h1>haha</h1></body></html>";
-
+        this.setContent("<html><body><h1>hello world</h1></body></html>");
+        //创建一个返回对象
         FullHttpResponse httpResponse = this.build();
-
+        //返回给用户
         ctx.writeAndFlush(httpResponse);
-
+        //断开连接
         writeFinish(ctx);
 
     }
 
+    //设置返回主体
+    public void setContent(String content){
+        this.content = content;
+    }
 
-
-
+    //设置返回状态码 默认200
+    public void setStatus(int code){
+        this.statusCode = code;
+    }
 
     /**
      *  创建一个自己，构造一个基于（netty的DefaultFullHttpResponse）的Response对象
@@ -65,8 +70,15 @@ public class SkyResponse {
      * 通过自身数据构造一个需要写回用户的Response对象，
      */
     private FullHttpResponse build(){
-        FullHttpResponse httpResponse = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, OK, Unpooled.copiedBuffer(content, CharsetUtil.UTF_8));
 
+        HttpResponseStatus status = HttpResponseStatus.valueOf(this.statusCode);
+
+        FullHttpResponse httpResponse =
+                new DefaultFullHttpResponse(
+                        HttpVersion.HTTP_1_1,
+                        status,
+                        Unpooled.copiedBuffer(content, CharsetUtil.UTF_8)
+                );
 
         return httpResponse;
     }
